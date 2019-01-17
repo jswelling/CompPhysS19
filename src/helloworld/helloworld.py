f9b8263beeab180a7c5966c57234ed2a3aba77b2
Joel Welling <welling@psc.edu>
2019-01-16 22:51:16 -0500

    Initial version of the 'helloworld' app

---
+++ src/helloworld/helloworld.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+'''
+Created on Jan 16, 2019
+@author: welling
+'''
+
+from __future__ import print_function
+import six
+
+def main():
+    print("""
+    Hello World, from...
+    Joel S. Welling
+    """)
+
+if __name__ == "__main__":
+    main()
